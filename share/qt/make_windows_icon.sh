9729dec108d2197be512b84fec0ee972fe97b858
bytebar <kochre@gmail.com>
2013-05-05 19:41:13 -0600

    Update make_windows_icon.sh

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/bitbar.ico
+ICON_DST=../../src/qt/res/icons/bytebar.ico
 
-convert ../../src/qt/res/icons/bitbar-16.png ../../src/qt/res/icons/bitbar-32.png ../../src/qt/res/icons/bitbar-48.png ${ICON_DST}
+convert ../../src/qt/res/icons/bytebar-16.png ../../src/qt/res/icons/bytebar-32.png ../../src/qt/res/icons/bytebar-48.png ${ICON_DST}
